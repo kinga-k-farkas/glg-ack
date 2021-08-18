72cae2008edf1639119d5121734644c5d59f6879
Andrew Minh Nguyen <amqdnguyen@gmail.com>
2021-08-18 23:54:10 +0000

    More EDA, model iteration, and refactoring

--- src/data/utils.py
+++ src/data/utils.py
@@ -7,6 +7,8 @@ def get_domains_and_questions(split, dataset:str):
     }
     return ds_map[dataset](split)
 
+## Grail QA
+
 def get_grail_qa(split) -> dict:
     if split not in ('train', 'dev'):
         raise(ValueError("`split` must be one of 'train' or 'dev'"))
@@ -23,8 +25,32 @@ def get_grail_qa(split) -> dict:
 
     return {'domains': domains, 'questions': questions}
 
-def set_domains(data, domains):
+def make_grail_qa():
+    train = pd.DataFrame(get_domains_and_questions('train', 'grail_qa'))
+    dev   = pd.DataFrame(get_domains_and_questions('dev',   'grail_qa'))
+
+    domains = ['medicine', 'computer', 'spaceflight', 'biology', 'automotive', 'internet', 'engineering']
+    train = filter_domains(train, domains)
+    dev   = filter_domains(dev,   domains)
+
+    healthcare_subdomains = ['medicine', 'biology']
+    technology_subdomains = ['computer', 'spaceflight', 'automotive', 'internet', 'engineering']
+
+    train = set_label(train, 'healthcare', healthcare_subdomains)
+    train = set_label(train, 'technology', technology_subdomains)
+    dev   = set_label(dev,   'healthcare', healthcare_subdomains)
+    dev   = set_label(dev,   'technology', technology_subdomains)
+
+    return train, dev
+
+##
+
+def filter_domains(data, domains):
     parts = []
     for domain in domains:
         parts.append(data.loc[data.domains == domain])
     return pd.concat(parts)
+
+def set_label(df, label, subdomains):
+    df.domains.loc[df.domains.isin(subdomains)] = label
+    return df
